8bef9b26653f40bee99ff74aeae70766c020f0f6
armin-akhtarzand <armin.akhtarzand@outlook.com>
2026-08-24 17:37:43 +0200

    add cart logic to JS and cart button in menu

--- script/app.js
+++ script/app.js
@@ -1,8 +1,20 @@
 const productButtons = document.querySelectorAll(".productButton");
+const cart = {};
 
 productButtons.forEach((button) => {
-    button.addEventListener("click", () =>{
-        const name = button.dataset.name;
-        alert(`Vara lagd i varukorg: KMP ${name}`);
-    } )
-})
+  button.addEventListener("click", () => {
+    const product = button.closest(".prod, .featured-prod");
+    const quantityInput = product.querySelector(".quantity");
+    const quantity = Number(quantityInput.value);
+    const name = button.dataset.name;
+
+    if (cart[name]) {
+      cart[name] += quantity;
+    } else {
+      cart[name] = quantity;
+    }
+
+    console.log(cart);
+    alert(`Vara lagd i varukorg: KMP ${name}`);
+  });
+});
