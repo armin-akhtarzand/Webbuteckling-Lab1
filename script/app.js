3e6b086e75bbd42757c2e4841ba26a67349e5a02
armin-akhtarzand <armin.akhtarzand@outlook.com>
2026-08-25 19:15:16 +0200

    render products from JSON using fetch

--- script/app.js
+++ script/app.js
@@ -1,25 +1,126 @@
-const productButtons = document.querySelectorAll(".productButton");
-const productItem = {};
-
-productButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    const product = button.closest(".prod, .featured-prod");
-    const quantityInput = product.querySelector(".quantity");
-    const quantity = Number(quantityInput.value);
-    const name = button.dataset.name;
-
-    if (productItem[name]) {
-      productItem[name] += quantity;
-    } else {
-      productItem[name] = quantity;
+const productList = document.getElementById("productList")
+const getData = async () => {
+  try{
+    const response = await fetch("./json/products.json");
+    if(!response.ok){
+      console.error("Fel från servern " + response.status)
     }
 
-    renderCart();
+    const products = await response.json();
+    renderProducts(products);
+  
+  }
+  catch(error){
+    console.error("Fel: ", error);
+  }
+}
 
-    console.log(productItem);
-    alert(`Vara lagd i varukorg: KMP ${name}`);
-  });
-});
+const renderProducts =(products) => {
+  products.forEach((product) =>{
+    console.log("Product: ", product)
+    const article = document.createElement("article");
+    article.classList.add("prod");
+    const name = document.createElement("h3")
+    name.textContent = product.name;
+    const image = document.createElement("img");
+    image.src = product.img;
+    image.alt = product.imgAlt;
+    const description = document.createElement("p")
+    description.textContent = product.description;
+    const voltage = document.createElement("p");
+    voltage.textContent = product.voltage;
+
+    const buySection = document.createElement("div");
+    buySection.classList.add("buy-section");
+    const quantity = document.createElement("input");
+    quantity.classList.add("quantity");
+    quantity.type = "number";
+    quantity.value = "1";
+    quantity.min = "1";
+    quantity.ariaLabel = "Product quantity for " + product.name;
+    const productButton = document.createElement("button");
+    productButton.classList.add("productButton")
+    productButton.dataset.name = product.name
+    productButton.textContent = "Lägg i varukorg"
+
+    const price = document.createElement("p");
+
+    if(product.oldPrice){
+      const oldPrice = document.createElement("span")
+      oldPrice.classList.add("old-price");
+      oldPrice.textContent = product.oldPrice;
+
+      const newPrice = document.createElement("span");
+      newPrice.classList.add("new-price");
+      newPrice.textContent = product.price;
+
+      price.appendChild(oldPrice);
+      price.appendChild(newPrice);
+    }else{
+      price.textContent = product.price;
+    }
+
+     
+    if(product.badge){
+      const badge = document.createElement("span");
+      badge.classList.add("badge")
+
+      if(product.badge === "REA"){
+        badge.classList.add("sale")
+      }else if(product.badge === "Nyhet"){
+        badge.classList.add("new")
+      }else if(product.badge === "Populär"){
+        badge.classList.add("popular")
+      }
+
+      badge.textContent = product.badge;
+      article.appendChild(badge)
+    }
+
+   
+
+    productButton.addEventListener("click", () => {
+      const productElement = productButton.closest(".prod");
+
+      const quantityInput = productElement.querySelector(".quantity");
+      const quantity = Number(quantityInput.value);
+      const name = productButton.dataset.name;
+
+      if(productItem[name]){
+        productItem[name] += quantity;
+      }else{
+        productItem[name] = quantity;
+      }
+
+      renderCart();
+
+      alert(`Vara lagd i varukorg: ${name}`)
+
+
+    })
+    
+    
+
+  
+
+
+    buySection.appendChild(quantity)
+    buySection.appendChild(productButton)
+    article.appendChild(name)
+    article.appendChild(image)
+    article.appendChild(description)
+    article.appendChild(voltage)
+    article.appendChild(price)
+    article.appendChild(buySection)
+    productList.appendChild(article)
+
+
+  })
+}
+getData();
+
+const productButtons = document.querySelectorAll(".productButton");
+const productItem = {};
 
 function toggleCart() {
   cart.classList.toggle("hidden");
@@ -34,7 +135,7 @@ function renderCart() {
   for (const item in productItem) {
     cartList.innerHTML += `
     <div class="cart-item">
-        <span>KMP ${item}</span>
+        <span>${item}</span>
         <span>${productItem[item]} st</span>
     </div>
 `;
